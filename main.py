8b945365330f1c92b588b307e04ceac10f707da4
Seymur Guseynov <SeymurG@R4362-SeymurG.local>
2022-07-05 14:30:01 +0400

    add element in the end of linked list

--- main.py
+++ main.py
@@ -12,12 +12,22 @@ class LinkedList:
         node = Node(data, self.head)
         self.head = node
 
+    def in_the_end(self,data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+        return
+
     def print(self):
         if self.head is None:
             print("Linked List is empty")
 
         itr = self.head
-        print(type(itr))
         llstr = ''
         while itr:
             llstr += str(itr.data) + '--->'
@@ -26,7 +36,9 @@ class LinkedList:
 
 if __name__ == '__main__':
     ll = LinkedList()
+    ll.in_the_end(1)
     ll.at_begining(5)
     ll.at_begining(90)
     ll.at_begining(110)
+    ll.in_the_end(2)
     ll.print()
